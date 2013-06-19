7e487905cca42d209d9158481a2a21b44c30cbef
Michael Williams <dinosaur@riseup.net>
2013-06-19 16:12:51 -0700

    [auth] fixed lazy property definition

--- resources/creature/view/index.js
+++ resources/creature/view/index.js
@@ -5,6 +5,8 @@ module['exports'] = function(options, callback) {
   var $ = this.$,
       creature = resource.use('creature');
 
+  console.log(options.request.user);
+
   creature.view.get.detailed.present(options, function(err, result) {
     if (err) { callback(err); }
 
