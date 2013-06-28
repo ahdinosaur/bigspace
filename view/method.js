0ef506bf28e2d6a6c72e4204bd8dd357df9c1389
Michael Williams <dinosaur@riseup.net>
2013-06-27 19:59:22 -0700

    [view] made sideBar nav for method

--- view/method.js
+++ view/method.js
@@ -2,27 +2,37 @@
 module['exports'] = function (options, callback) {
 
   var resource = require('resource'),
+      html = require('html-lang'),
       forms = resource.use('forms'),
       rName = options.resource || 'space',
+      method = options.method || 'all',
       display = options.display || 'index',
       r = resource.use(rName),
       $ = this.$,
       _view = this;
 
-  // TODO add methods to sidebar
-  // r.methods
+  // add methods to sidebar
+  var tmpl = '<li class="sideTab"><a class="sideLink" href=""></a></li>';
+  Object.keys(r.methods).sort().forEach(function(methodName) {
+    $('#sideNav').append(html.render({
+      'sideTab.class': (methodName === method) ? 'active' : undefined,
+      'sideLink': methodName,
+       // TODO append id (if any) to href
+      'sideLink.href': "/" + rName + "/" + methodName
+    }, tmpl));
+  });
 
   // either delegate to resource view (if exists)
   // or default to using forms
   var delegate;
   // check if resource view exists
   if ((typeof r.view !== 'undefined') &&
-    (typeof r.view[options.method] !== 'undefined') &&
-    (typeof r.view[options.method][display] !== 'undefined')) {
+    (typeof r.view[method] !== 'undefined') &&
+    (typeof r.view[method][display] !== 'undefined')) {
     // TODO get rid of this hack because calling a view
     // directly shouldn't be a problem
     delegate = function(options, callback) {
-      var viewObj = r.view[options.method][display];
+      var viewObj = r.view[method][display];
       return viewObj.present.call(viewObj, options, callback);
     };
   } else {
