c2424f6ed2165a9cb1fad9436c3ec8929deaf480
Daniel Leja <wolfbiter@gmail.com>
2013-06-28 20:09:09 -0700

    [forms] [minor] removed unnecessary declarations

--- resources/forms/view/form/all.js
+++ resources/forms/view/form/all.js
@@ -2,11 +2,7 @@ var resource = require('resource'),
     async = require('async');
 
 module['exports'] = function (options, callback) {
-
   options = options || {};
-  var $ = this.$,
-    self = this,
-    r = resource.resources[options.resource];
 
   // all never needs additional data and always posts
   options.action = 'post';
