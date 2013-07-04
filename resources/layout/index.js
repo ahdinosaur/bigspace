e9087d46fd8e2fe826367183c93d3154b81edc9c
Michael Williams <dinosaur@riseup.net>
2013-07-04 02:11:56 -0700

    [layout] changed convention for layout view from layout.view.layout to layout.view

--- resources/layout/index.js
+++ resources/layout/index.js
@@ -9,11 +9,15 @@ layout.persist('memory');
 // .init() convention
 function init(callback) {
   // setup .view convention
-  var view = resource.use('view');
-  view.create({ path: __dirname + '/view' }, function(err, _view) {
-      if (err) { return callback(err); }
-      layout.view = _view;
-      return callback(null, _view);
+  var view = resource.use('view'),
+      fs = require('fs');
+  view.create({
+    template: fs.readFileSync(__dirname + '/view/layout.html'),
+    presenter: require('./view/layout')
+  }, function(err, _view) {
+    if (err) { return callback(err); }
+    layout.view = _view;
+    return callback(null, _view);
   });
 }
 layout.method('init', init, {
