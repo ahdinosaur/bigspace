adb468cbc68430bee31d0d71adefc640c7fdd429
Michael Williams <dinosaur@riseup.net>
2013-07-11 15:42:48 -0700

    [layout] fixed creation of layout view

--- resources/layout/index.js
+++ resources/layout/index.js
@@ -12,8 +12,8 @@ function init(callback) {
   var view = resource.use('view'),
       fs = require('fs');
   view.create({
-    template: fs.readFileSync(__dirname + '/view/layout.html'),
-    presenter: require('./view/layout')
+    templatePath: __dirname + '/view/layout.html',
+    presenterPath: __dirname + '/view/layout'
   }, function(err, _view) {
     if (err) { return callback(err); }
     layout.view = _view;
